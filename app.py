4fa70ed924ac43e465f611d1af2c9406e75cc331
dmytro <dzeleniy9@gmail.com>
2020-11-05 23:07:28 +0200

    Initial commit and add some html file

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+
+@app.context_processor
+def override_url_for():
+    return dict(url_for=dated_url_for)
+
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
+
+
+if __name__ == '__main__':
+    app.run()
